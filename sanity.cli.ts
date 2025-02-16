84240362f8a178f675dc81ba061352cff25a9346
Mihira Jayasekera <mihira@tinymachine.net>
2025-02-15 20:45:05 -0800

    Revert "chore: set sanity studio to auto-update"
    This reverts commit b0856b7792633ed868a4793ad19f0133ed7506d2.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,9 +1,8 @@
-import { defineCliConfig } from 'sanity/cli'
+import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'oqzk4lvk',
     dataset: 'production'
-  },
-  autoUpdates: true
+  }
 })
